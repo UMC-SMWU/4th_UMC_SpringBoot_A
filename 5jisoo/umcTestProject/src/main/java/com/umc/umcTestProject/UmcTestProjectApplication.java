ae0dda3f5171a0f869c51e263b7a8e4384e3ff7e
5jisoo <56earls@gmail.com>
2023-05-10 18:06:28 +0900

    [Feat(콜린/오지수)] Project Settings

---
+++ 5jisoo/umcTestProject/src/main/java/com/umc/umcTestProject/UmcTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.umc.umcTestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcTestProjectApplication.class, args);
+	}
+
+}
